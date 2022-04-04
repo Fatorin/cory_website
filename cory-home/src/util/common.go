ebeeb9768fcc4172608dfb5b1dd389cd49e2a1fd
Fatorin <sksly789@gmail.com>
2022-04-05 01:51:52 +0800

    Rename redis key to common
    Rename redis key to common

--- cory-home/src/util/common.go
+++ cory-home/src/util/common.go
@@ -1,8 +1,12 @@
 package util
 
+// Redis Key
 const APPTEXT_CACHE string = "APP_TEXT_CACHE"
 const LANGUAGES_CACHE string = "LANGUAGES_CACHE"
 const IMAGE_CACHE string = "IMAGE_CACHE"
 const TWITCH_CACHE_CHEERS string = "TWITCH_CACHE_CHEERS"
 const TWITCH_CACHE_EMOTES string = "TWITCH_CACHE_EMOTES"
 const TWITCH_CACHE_BADGES string = "TWITCH_CACHE_BADGES"
+
+// Common String
+const IMAGE_FOLDER_PATH string = "./images/"
