d45c2335ef554314e2f8c2f3ff600e37fe35cdcb
Fatorin <sksly789@gmail.com>
2022-05-04 16:15:21 +0800

    implement Component api.
    implement Component api.

--- cory-home/src/models/component.go
+++ cory-home/src/models/component.go
@@ -3,7 +3,6 @@ package models
 type Component struct {
 	Model
 	Type          uint   `json:"type"`
-	Order         uint   `json:"order"`
 	Image         string `json:"image"`
 	Text          string `json:"text"`
 	CustomSetting string `json:"custom_setting"`
